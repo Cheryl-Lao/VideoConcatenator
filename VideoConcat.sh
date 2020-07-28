0e8915152a484b5a4d4eefea160efdd1e12e3ee8
Cheryl Lao <cheryl.lao@mail.utoronto.ca>
2020-07-27 21:26:28 -0400

    Added line to clear the file with the list of concatenated files before each iteration

--- VideoConcat.sh
+++ VideoConcat.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+> filesToConcatenate.txt;
 for f in ./$1/*; do echo "file '$f'" >> filesToConcatenate.txt; done
 ffmpeg -f concat -safe 0 -i filesToConcatenate.txt -c copy $1Concatenated.mp4
